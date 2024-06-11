e55abc966b9f095e7a807ce4293ac919d466e905
LyDucAnh <namobilegame@gmail.com>
2024-06-11 11:57:20 +0700

    udpate install and upgrade script

--- Packt/SEO/registration.php
+++ Packt/SEO/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Packt_HelloWorld',
+    'Packt_SEO',
     __DIR__
 );
